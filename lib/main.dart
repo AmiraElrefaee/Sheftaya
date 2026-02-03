be7d3062c25ed99306fa9f92ee1bd68461bfbe96
amira hamed <amirahamed173@gmail.com>
2026-02-03 02:28:20 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'feature/home/presentation/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+      ),
+      home: const HomeView(),
+    );
+  }
+}
+
